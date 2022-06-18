eb2c746b8e67b92520b5322fd37d6a21b67c8f55
Gurubaran-B <guru_baran@icloud.com>
2022-06-18 08:20:20 +0530

    added lottie

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -57,6 +57,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -131,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1562e5febb3f8182"; }
+/******/ 		__webpack_require__.h = function() { return "72e0561ec588413a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/Reward.js
+++ pages/Reward.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import RewardCard from '../Components/RewardCard.js'
+import Lottie from 'react-lottie';
+import muscle from '../asset/Lottie/muscle.json';
 import styles from '../styles/Reward.module.css';
 
 function Reward() {
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: muscle,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
     return (
         <div>
             <div className={styles.container}>
@@ -10,7 +22,12 @@ function Reward() {
                     <div className={styles["inner"] + " " + styles["circle"]} >
                         <div className={styles.field}>
                             <div className={styles.ellipse}><img src='Ellipse.svg'/></div>
-                            <div className={styles.emoj}><img src='muscle.gif'/></div>
+                            <div className={styles.emoj}>
+                                <Lottie
+                                    options={defaultOptions}
+                                    height={100}
+                                    width={100}/>
+                            </div>
                             <div className={styles.globe}><img src='Globe.svg'/></div>
                             <div className={styles.cube}><img src='Cube.svg'/></div>
                         </div>
